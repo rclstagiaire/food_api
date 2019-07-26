a5a1573b7c5e521c6468ed2c7372bd57264c8f8e
FOMOjahaelle <jahaellefomo92@gmail.com>
2019-07-26 19:25:44 +0100

    merge create_table

--- server/create_table.js
+++ server/create_table.js
@@ -6,29 +6,21 @@ var lbTables = [
   'ACL',
   'RoleMapping',
   'Role',
-<<<<<<< HEAD
   'notification',
   'customer',
   'delivers',
   'orders',
-  'ligne-commande'];
-=======
-  'rolemembre',
-  'utilisateur',
+  'ligne_commande',
   'categories',
   'menus',
   'products',
   'restaurants',
-  'menuItem'
+  'menu_item',
+  'payment_mode'
 ];
->>>>>>> 45f394830c83307b6904ba4680ca05aef459b69d
 ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' - lbTables - '] created in '
     , ds.adapter.name);
   ds.disconnect();
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 45f394830c83307b6904ba4680ca05aef459b69d
